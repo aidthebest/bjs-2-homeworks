f6fc9865d3073c304e2f027ee4228f3b104531ee
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-09-21 22:05:03 +0300

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,23 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+
+  switch (d) {
+    case (d < 0):
+      arr.push();
+      break;
+
+    case (d === 0 ):
+      arr.push(-b / (2*a))
+      break;
+
+    case (d > 0):
+      arr.push(-b + Math.sqrt(d) / (2 * a));
+      break;
+  }
+
   return arr; // array
 }
 
