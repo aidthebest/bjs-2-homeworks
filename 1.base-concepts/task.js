fe516291f0b52c1bdb068e45ab7323926f71be37
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-09-22 14:31:25 +0300

    2nd tas

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,7 +19,36 @@ function solveEquation(a, b, c) {
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
 
-  // код для задачи №2 писать здесь
+  if (isNaN(percent)) {
+    return totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+  } else if (isNaN(contribution)) {
+    return totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+  } else if (isNaN(amount)) {
+    return totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+  }
+
+
+  // let s = amount - contribution;
+  // let dateToday = new Date();
+  // let month = date.getUTCMonth() - dateToday.getUTCMonth() + (date.getFullYear() - dateToday.getFullYear()) * 12;
+  // let monthlyPayment = s * (percent / 12 / 100 + (percent / 12 / 100) / (((1 + percent / 12 / 100) ** month) - 1));
+  // totalAmount = Number((monthlyPayment * month).toFixed(2));
+  // console.log(totalAmount);
+  // return totalAmount;
+
+
+
+
+
+  let creditBody = amount - contribution;
+
+  let creditStartDate = new Date();
+
+  let loanDurationInMonth = (date.getFullYear() - creditStartDate.getFullYear()) * 12 + creditStartDate.getMonth() - date.getMonth();  
+
+  let mounthlePayment = creditBody * (percent / 12 / 100 + (percent / 12 / 100) / (((1 + percent / 12 / 100) ** loanDurationInMonth) - 1));
+
+  totalAmount = +((loanDurationInMonth * mounthlePayment).toFixed(2));
 
   return totalAmount;
 }
