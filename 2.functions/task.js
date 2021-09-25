e1947ad5c19630df061504204ffc41d2fab22192
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-09-25 12:23:36 +0300

    functions

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+  let min = Infinity, max = -Infinity, sum = 0, avg;
 
-  // Ваш код
+  let i
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr [i];
+    }
+    if (arr[i] > max) {
+      max = arr [i];
+    }
+    sum += arr[i];
+  }
+
+  avg = +(sum / arr.length).toFixed(2);
 
   return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return +sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
-  // for ...
-  
+  let max = -Infinity;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (max < func(arrOfArr[i])) {
+      max = func(arrOfArr[i]);
+    }
+  }
   return max
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let difference = getArrayParams(arr);
+  return Math.abs(difference.max - difference.min);
 }
