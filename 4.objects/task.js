4a0c9001b8e812eb8a7a850846aade3f99763af9
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-10-02 12:19:07 +0300

    objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name,
+  this.gender = gender,
+  this.age = age
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+}
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(...mark);
+  } else {
+    this.marks.push(...mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = this.marks.reduce((total, amount) => total + amount);
+  return (sum / this.marks.length);
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
