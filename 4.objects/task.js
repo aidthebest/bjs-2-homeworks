2e82c60372fba0daa21184daf22ccb4cd3b04588
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-10-07 15:55:44 +0300

    classes

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,33 +1,47 @@
-function Student(name, gender, age) {
-  this.name = name,
-  this.gender = gender,
-  this.age = age
+// function Student(name, gender, age) {
+//   this.name = name,
+//   this.gender = gender,
+//   this.age = age
+// }
+
+// Student.prototype.setSubject = function (subjectName) {
+//   this.subject = subjectName;
+// }
+
+// Student.prototype.addMark = function (mark) {
+//   if (this.marks === undefined) {
+//     this.marks = [];
+//   }
+//   this.marks.push(mark);
+// }
+// Student.prototype.addMarks = function (...mark) {
+//   if (this.marks === undefined) {
+//     this.marks = [];
+//   }
+//   this.marks.push(...mark);
+// }
+
+// Student.prototype.getAverage = function () {
+//   let sum = this.marks.reduce((total, amount) => total + amount);
+//   return (sum / this.marks.length);
+// }
+
+// Student.prototype.exclude = function (reason) {
+//   delete this.subject;
+//   delete this.marks;
+//   this.excluded = reason;
+// }
+
+let array = [20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 10240, 20480,  40960]
+
+
+function reallySumm(arr){
+  let sum = 0
+  for (let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+return sum;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  this.subject = subjectName;
-}
-
-Student.prototype.addMark = function (mark) {
-  if (this.marks === undefined) {
-    this.marks = [];
-  }
-  this.marks.push(mark);
-}
-Student.prototype.addMarks = function (...mark) {
-  if (this.marks === undefined) {
-    this.marks = [];
-  }
-  this.marks.push(...mark);
-}
-
-Student.prototype.getAverage = function () {
-  let sum = this.marks.reduce((total, amount) => total + amount);
-  return (sum / this.marks.length);
-}
-
-Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
-}
+let s = reallySumm(array)
+console.log(s);
---
+++ 5.classes/task.js
@@ -0,0 +1,66 @@
+class PrintEditionItem {
+
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state = this.state * 1.5;
+        if (this.state > 100) {this.state = 100};
+        return this.state
+    }
+
+    set state(number) {
+        if (number < 0) {number = 0} 
+        else if(number > 100) {number = 100}
+        else number = number
+        this._state = number
+    }
+
+    get state() {
+        return this._state
+    }
+}
+
+class Magazine extends PrintEditionItem() {
+
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "magazine";    
+    }   
+}
+
+class Book extends PrintEditionItem() {
+
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";    
+    }   
+}
+
+class Novelbook extends Book() {
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = "novel";
+    }
+}
+
+class Fantastic extends Book() {
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = "fantastic";
+    }
+}
+
+class Detective extends Book() {
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = "detective";
+    }
+}
