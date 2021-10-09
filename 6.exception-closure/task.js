6f62bee0139e9648db7525b4047cc6c10f4d6bc2
aidthebest <82460114+aidthebest@users.noreply.github.com>
2021-10-09 17:51:31 +0300

    initial commit

---
+++ 6.exception-closure/task.js
@@ -0,0 +1,51 @@
+function parseCount (count) {
+    let res = Number.parseInt(count);
+    if (Number.isNaN(res)) {
+        throw new Error("Невалидное значение");
+    };
+    return res;
+}
+
+function validateCount(count) {
+    try {
+        const a = parseCount(count)
+        return a;
+    }catch(err) {
+        return err; 
+    }finally {
+    }  
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        if (((a + b) < c) || ((a + c) < b) || ((b + c ) < a)) {
+            throw new Error("Треугольник с такими сторонами не существует");
+          }
+        }
+
+    getPerimeter()  {
+        let per = this.a + this.b + this.c;
+        return per
+    }
+
+    getArea() {
+        let p = this.getPerimeter / 2;
+        let s = Math.sqrt(p*(p - this.a)*(p - this.b)*(p - this.c)).toFixed(3);
+        return s;
+    }  
+}
+
+function getTriangle(a, b, c) {
+    try {
+        let newTriangle = new Triangle(a, b, c);
+        return newTriangle;
+    }catch (err) {
+        // return ?????????;
+    } finally  {
+    }
+    return 
+    // {getPerimeter = () => {return "Ошибка! Треугольник не существует"}, getArea = () => {return "Ошибка! Треугольник не существует"}};
+}
