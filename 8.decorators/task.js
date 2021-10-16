fc20f489a5e08a951b319868ec575577f02ce36f
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-10-16 19:42:15 +0300

    decor 1 v1.0

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,26 @@
 function cachingDecoratorNew(func) {
-  // Ваш код
+  
+  let cache = []
+  function wrapper(...args) {
+
+    let key = args.toString();
+    console.log(cache);
+    if (key in cache) {
+      return "Из кэша: " + cache[key]
+    } else {
+      let result = func(...args);
+      cache[key] = result;
+      cache.push(func(...args));
+      if (cache.length > 5) {
+        cache.splice(0, 1);
+      }
+      console.log(cache);
+      return "Вычисляем: " + result;
+    }
+
+  }
+
+  return wrapper
 }
 
 
