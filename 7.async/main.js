ac6b9654666fa45316aeb7d3798545874ba81578
Alexander Mizyukin <a.mizyukin@mail.ru>
2021-10-16 11:52:45 +0300

    async

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,24 @@
-// тут вы можете вызывать функции из task.js
+function testCase () {
+    clock = new AlarmClock();
+
+    let t = clock.getCurrentFormattedTime();
+
+    clock.addClock(t, () => console.log("ping-pong"),1);
+
+    clock.addClock((t + ??), () => {
+        console.log("ping-pong 2");
+        clock.removeClock(2);
+    }, 2);
+
+    clock.addClock((t + ??), () => {
+        console.log("ping-pong 3");
+        clock.stop();
+        clock.clearAlarms();
+        clock.printAlarms();
+    }, 3);
+
+    clock.printAlarms();
+
+    clock.start();
+    
+}
